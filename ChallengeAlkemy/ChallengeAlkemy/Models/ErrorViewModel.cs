0cbdb3b1ea2c9271b12fe390ae6f96a3d0418a90
jhernandez <jhernandez@grupomurchison.com.ar>
2022-05-05 19:47:51 -0300

    Inicial

---
+++ ChallengeAlkemy/ChallengeAlkemy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChallengeAlkemy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
